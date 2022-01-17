53b45f136bff23b5b962f9d4322aa8785e591b8f
Aaronuneal <90298033+Aaronuneal@users.noreply.github.com>
2022-01-17 18:36:48 +0800

    Update MainWindow.xaml.cs

--- Version4/UserInterface/MainWindow.xaml.cs
+++ Version4/UserInterface/MainWindow.xaml.cs
@@ -11,51 +11,50 @@ namespace version4.UserInterface
 {
     public partial class mainwindow : Window
     {
-        public int OriginalX; //      原来坐标                                              //声明需要的全局变量，与实例化
+        public int OriginalX; // These two variables are the original coordinate of a chosen chess                                             
         public int OriginalY;
-        public int currentX;//移动后的坐标
+        public int currentX;// These two variables are the goal coordinate to move to of a chosen chess
         public int currentY;
 
-        public bool firstClicked=true;//to check if it is the firstclick
-        public int nbCount = (int)Chess.Player.red;//回合方
-        public int nbCall=0;//回合数
+        public bool firstClicked=true;//to check if it is the firstclick,is used for telling whether the click aims to choose a chess to move or to move a chosen chess
+        public int nbCount = (int)Chess.Player.red;// To tell whether it's red side turn or black side turn
+        public int nbCall=0;// The number of round(how many moves have been set)
         public Chess[,] chessMatrix;
         public Chess[,] path;
-        ProgramModel Mod = new ProgramModel();//实例化model模块
-        ProgramControl Con = new ProgramControl();//实例化控制模块
+        ProgramModel Mod = new ProgramModel();// instantiate the model module
+        ProgramControl Con = new ProgramControl();// instantiate the control module
 
-        //主函数
         public mainwindow()
         {
             InitializeComponent();
             BackGroundMusic();// 背景音乐
-            chessMatrix = Mod.setPosition();                 //初始化当前的矩阵，即实际的棋盘按钮矩阵
-            path = Mod.SetRoad();                           //初始化棋子的路径
-            CreateGrid(chessMatrix);                            //打印棋盘与棋子
+            chessMatrix = Mod.setPosition();                 // initialize the chess board matrix
+            path = Mod.SetRoad();                           // initialize the path of the chess
+            CreateGrid(chessMatrix);                            // Print out the chess and chess board 
         }
 
         public void CreateGrid(Chess[,] currentMatrix)
         {
-            Grid MotherGrid = new Grid();                                    //创建背景的boardGrid 1
+            Grid MotherGrid = new Grid();                                    // create the background grid
             this.Content = MotherGrid;
-            Grid boardGrid = new Grid();                                     //创建棋盘的boardGrid 1
-            MotherGrid.Children.Add(boardGrid);                                  //在totalGrid里添加boardGrid 1
-            boardGrid.HorizontalAlignment = HorizontalAlignment.Left;           //使boardGrid打开在左方 1
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;         //让窗口在屏幕中央打开 1
+            Grid boardGrid = new Grid();                                     // create the chess board grid
+            MotherGrid.Children.Add(boardGrid);                                  
+            boardGrid.HorizontalAlignment = HorizontalAlignment.Left;           // set the position of the chess board grid
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;         
             
             for (int i = 0; i < 9; i++)
             {
-                boardGrid.ColumnDefinitions.Add(new ColumnDefinition());//列的初始化
+                boardGrid.ColumnDefinitions.Add(new ColumnDefinition());
             }
 
             for (int i = 0; i < 10; i++)
             {
-                boardGrid.RowDefinitions.Add(new RowDefinition());//行的初设化
+                boardGrid.RowDefinitions.Add(new RowDefinition());
             }
-            CreateLayout(currentMatrix, boardGrid);                                                //打印布局，每次都要重新布局
+            CreateLayout(currentMatrix, boardGrid);                                                // print out the layout, declared below
         }
 
-        public void CreateLayout(Chess[,] currentMatrix, Grid boardGrid)                        //布局，布置棋子
+        public void CreateLayout(Chess[,] currentMatrix, Grid boardGrid)                        // called above
         {
             Button[,] btn = new Button[10, 9];
             ImageBrush brush = new ImageBrush();
@@ -65,28 +64,28 @@ namespace version4.UserInterface
             {
                 for (int row = 0; row < 9; row++)
                 {
-                    btn[col, row] = new Button();                                   //设置棋子按钮的各种属性，棋盘中的每个棋子都是一个按钮
+                    btn[col, row] = new Button();                                   // set the attributes of each chess and set them as buttons
                     btn[col, row].Width = 30;
                     btn[col, row].Height = 30;
                     btn[col, row].Margin = new Thickness(11, 3, -1, 3);
                     btn[col, row].BorderThickness = new Thickness(0, 0, 0, 0);
                     btn[col, row].Background = Brushes.Transparent;         //匹配parent（即前一个）的背景
                     btn[col, row] = LoadPieces(currentMatrix, btn[col, row], col, row);//一个一个加载图片
-                    btn[col, row].SetValue(Grid.RowProperty, col);//这个我也不清楚，可以网上查查setvalue的作用
+                    btn[col, row].SetValue(Grid.RowProperty, col);
                     btn[col, row].SetValue(Grid.ColumnProperty, row);
-                    // 画出路径
+                    // draw the path of where the chosen chess can move to
                     if (path[col, row].path == Chess.Piecepath.yes)
                     {
-                        btn[col, row].Background = brush;   //画刷，即使之能画出路径
+                        btn[col, row].Background = brush;   
                     }
-                    // 把一个一个按钮加到boardgrid中
+                    // add the button on the grid
                     boardGrid.Children.Add(btn[col, row]);
                 }
             }
-            ButtonEvent(btn);         //赋予按钮事件      
+            ButtonEvent(btn);         // give the button events     
         }
 
-        public Button LoadPieces(Chess[,] currentMatrix, Button btn, int col, int row)           //加载各个棋子的图片
+        public Button LoadPieces(Chess[,] currentMatrix, Button btn, int col, int row)           // To load the images of the chesses
         {
             ImageBrush chessImage = new ImageBrush();
             switch (currentMatrix[col, row].side)
@@ -150,77 +149,77 @@ namespace version4.UserInterface
             return btn;
         }
 
-        public void ButtonClick(object sender, RoutedEventArgs e)              //创建点击事件
+        public void ButtonClick(object sender, RoutedEventArgs e)              // create the click event
         {
-            int btnRow = (int)((Button)sender).GetValue(Grid.RowProperty);//传递鼠标点击事件，具体可以网上看看getvalue是怎么用的
+            int btnRow = (int)((Button)sender).GetValue(Grid.RowProperty);        // pass the event by button
             int btnCol = (int)((Button)sender).GetValue(Grid.ColumnProperty);
             ClickEvent(btnRow, btnCol);
         }
 
-        public void ButtonEvent(Button[,] btn)              //给每个按钮添加这些点击事件
+        public void ButtonEvent(Button[,] btn)              // add event for the buttons
         {
             for (int col = 0; col < 10; col++)
                 for (int row = 0; row < 9; row++)
                     btn[col, row].Click += ButtonClick;
         }
 
-        public void ClickEvent(int btnRow, int btnCol)               //点击发生时进行事件的内容
+        public void ClickEvent(int btnRow, int btnCol)               // content of the event(what happen when click the button)
         {
             bool result;
-            try//这里讲的时候特别申明，可能有加分
+            try//这里讲的时候特别申明
             {
-                if (firstClicked)                                                            //第一次点击即选择棋子
+                if (firstClicked)   // to choose a chess to move
                 {
-                    OriginalX = btnRow;// 看选中哪个棋子
+                    OriginalX = btnRow;  // to tell which chess is chosen
                     OriginalY = btnCol;
                     if (chessMatrix[OriginalX, OriginalY].side == Chess.Player.blank)
                         MessageBox.Show("There is no piece!");
                     if (chessMatrix[OriginalX, OriginalY].side == Chess.Player.red || chessMatrix[OriginalX, OriginalY].side == Chess.Player.black)
                     {
                         if (nbCount != (int)chessMatrix[OriginalX, OriginalY].side)
-                            MessageBox.Show("It's not your turn");//在己方回合选择对方棋子
+                            MessageBox.Show("It's not your turn");
                         else
                         {
-                            path = Con.Road(OriginalX, OriginalY, chessMatrix);           //显示当前棋子可行路径
+                            path = Con.Road(OriginalX, OriginalY, chessMatrix);           // path is where the chosen chess can move to
                             firstClicked = false;
-                            CreateGrid(chessMatrix);                              //更新boardGrid
-                            path = Mod.SetRoad();                           //初始化路径       
+                            CreateGrid(chessMatrix);                              // update the chess board grid after choosing the chess
+                            path = Mod.SetRoad();                           // set the path as initialiazational value 
                         }
                     }
                 }
-                else          //选中了棋子开始选择移动时（第二次点击）
+                else          // to move a chosen chess
                 {
-                    currentX = btnRow;//移动后的按钮位置
+                    currentX = btnRow;  // the goal coordinate where the chess will move to
                     currentY = btnCol;
 
-                    if (currentX == OriginalX && currentY == OriginalY)                 //选中自己
+                    if (currentX == OriginalX && currentY == OriginalY)                 // if choose the original position
                     {
                         MessageBox.Show("Please try again!");
-                        CreateGrid(chessMatrix);                             //更新boardGrid
+                        CreateGrid(chessMatrix);                             // update the chess board grid
                     }
-                    else                                                        //成功选中
+                    else                                                        // sucessful move 
                     {
                         bool move = Con.MovePiece(currentX, currentY, OriginalX, OriginalY, chessMatrix);
                         if (!move) MessageBox.Show("You cannot move to there!");
                         else
                         {
-                            nbCall++;//回合数加一
-                            CreateGrid(chessMatrix);      //更新棋盘                         
-                            result = Con.Result(chessMatrix);                         //检查游戏是否结束
-                            if (result)                                    //判断结果，每次点击两次，即有棋子成功移动后就要进行判断，是否游戏已经结束了
-                            {   //游戏结束时若是红方回合，即证明胜利方为红方
-                                if (nbCount == (int)Chess.Player.red) MessageBox.Show("Red Win!!\n" + "You have used " + nbCall + " rounds to be finished."); //红方赢
-                                else MessageBox.Show("Black Win!!\n" + "You have used " + nbCall + " rounds to be finished.");                                         //黑方赢
-                                Environment.Exit(0);            //游戏结束自动退出程序
+                            nbCall++;// the number of round should plus 1
+                            CreateGrid(chessMatrix);      // update the chess board grid after moving the chess                         
+                            result = Con.Result(chessMatrix);                         
+                            if (result)                                    // to check if the game is over after every two clicks
+                            {   // when the game is over, the turn owner is winner
+                                if (nbCount == (int)Chess.Player.red) MessageBox.Show("Red Win!!\n" + "You have used " + nbCall + " rounds to be finished."); // red wins
+                                else MessageBox.Show("Black Win!!\n" + "You have used " + nbCall + " rounds to be finished.");                                // balck wins
+                                Environment.Exit(0);            // game over and exit
                             }
-                            if (nbCount == (int)Chess.Player.red)//每有棋子移动后就要进行回合的交换
+                            if (nbCount == (int)Chess.Player.red) // exchange the turn between red side and black side
                                 nbCount = (int)Chess.Player.black;
                             else
                                 nbCount = (int)Chess.Player.red;
                         }
 
                     }
-                    firstClicked = true;//第一次点击后成功选中棋子
+                    firstClicked = true; // if the first click is sucessful(choose a chess to move)
                 }
             }
             catch (OverflowException)
@@ -233,7 +232,7 @@ namespace version4.UserInterface
         {
             SoundPlayer sp = new SoundPlayer();
             sp.SoundLocation = "backgroundmusic\\LittleXiangQI.wav";
-            sp.PlayLooping();      //循坏播放      
+            sp.PlayLooping();      // to play the music over and over again    
         }
     }
 }
